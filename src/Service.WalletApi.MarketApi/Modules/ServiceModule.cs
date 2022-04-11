3e2ee978b5970c48d4c6c55b761ab2aca01f1717
dmitriy.b <dmitriy.b@smplt.net>
2022-04-11 16:19:32 +0500

    Use new wallet sdk

--- src/Service.WalletApi.MarketApi/Modules/ServiceModule.cs
+++ src/Service.WalletApi.MarketApi/Modules/ServiceModule.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Logging;
 using MyJetWallet.ApiSecurityManager.Autofac;
 using MyJetWallet.Sdk.RestApiTrace;
-using MyJetWallet.Sdk.Service;
 using Service.Market.Client;
 
 namespace Service.WalletApi.MarketApi.Modules
@@ -11,8 +10,7 @@ namespace Service.WalletApi.MarketApi.Modules
 	{
 		protected override void Load(ContainerBuilder builder)
 		{
-			// second parameter is null because we do not store api keys yet for wallet api
-			builder.RegisterEncryptionServiceClient(ApplicationEnvironment.AppName, () => Program.Settings.MyNoSqlWriterUrl);
+			builder.RegisterEncryptionServiceClient();
 
 			builder.RegisterMarketClient(Program.Settings.MarketServiceUrl, Program.LoggerFactory.CreateLogger(typeof (MarketClientFactory)));
 
